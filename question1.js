640085e8cd4fb81ac2776336224f75162dc8357d
Theosimon0828 <63924343+Theosimon0828@users.noreply.github.com>
2020-08-30 23:58:33 +0200

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,5 +1,5 @@
 //question 1
 
-// const division = (a, b) => a % b;
-// const result = division(5,2);
-// console.log(result);
+ const division = (a, b) => a % b;
+ const result = division(5,2);
+ console.log(result);
